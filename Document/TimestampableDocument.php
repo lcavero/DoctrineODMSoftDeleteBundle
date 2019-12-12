86c921bbe24eb20fd726f220325a3e92fdc9da73
Luis Cavero <luiscaverodeveloper@gmail.com>
2019-12-12 19:54:41 +0100

    rework

--- Document/TimestampableDocument.php
+++ Document/TimestampableDocument.php
@@ -6,6 +6,7 @@ namespace LCV\DoctrineODMSoftDeleteBundle\Document;
 use DateTime;
 use Doctrine\ODM\MongoDB\Mapping\Annotations as MongoDB;
 use JMS\Serializer\Annotation as Serializer;
+use Gedmo\Mapping\Annotation as Gedmo;
 
 /**
  * Class TimestampableDocument
@@ -22,6 +23,7 @@ abstract class TimestampableDocument
     /**
      * @var DateTime
      * @MongoDB\Field(type="date")
+     * @Gedmo\Timestampable(on="create")
      * @Serializer\Groups({"COMMON.created"})
      */
     protected $created;
@@ -29,6 +31,7 @@ abstract class TimestampableDocument
     /**
      * @var DateTime
      * @MongoDB\Field(type="date")
+     * @Gedmo\Timestampable(on="update")
      * @Serializer\Exclude()
      */
     protected $updated;
